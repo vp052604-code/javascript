5e39670eb82a042f13d9ef38e88226483dfc8fc4
prajapati vishnu <vp052604@gmail.com>
2025-12-22 16:03:27 +0530

    Add files via upload

--- indes.js
+++ indes.js
@@ -7,19 +7,54 @@ let gname='vishnu'
 // console.log(gname);
 // console.log(gname.charAt(3));//h
 // console.log(gname.indexOf('v'));//0
-// console.log(gname.toLocaleUpperCase());
+// console.log(gname.toLocaleUpperCase());  //VISHNU
 
-// const Gname =gname.substring(2, 5)
+// const Gname =gname.substring(2, 5) //shn
 // console.log(Gname);
 
 // const trime = "   oppo  ";
 // console.log(trime);
 // console.log(trime.trim());
 
-const url ='prajapari.vishnu';
+// const url ='prajapari.vishnu';
 // console.log(url.replace('.' ,'_'));
 // ==================================
 const map=100;
 
-console.log(map.toString().length);
-console.log(map.toFixed(2))
+// console.log(map.toString().length);
+// console.log(map.toFixed(2))//100.00
+
+// ==================================
+ //    vi
+// console.log("2">1)  //true
+// console.log("2.8">=1)  //true
+ 
+// console.log(null > 0)   //false
+// console.log(null < 0)   //false
+// console.log(null == 0)  //false
+// console.log(null >= 0)  //true
+
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+
+//  Data types ===
+// console.log(null===0)  //false
+
+
+// primitive
+// 7 types:- String,number,null,undefined,Boolean,Symbol,BigInt
+
+const score=100
+const scorevalue=100.23
+const islooggedin=false
+const outsideTemp=null
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id===anotherid)//  false
+console.log(id==anotherid)//  false
+
+// non primitive
+// Array,Objects,function
